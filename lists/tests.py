f6003a566b69d38d5e2a02ccdecf3d575ee9c3d6
dfe325 <daneder@gmail.com>
2020-05-25 07:57:52 -0400

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,7 @@
 #lists/tests.py
 
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
-from lists.views import home_page
 
 class HomePageTest(TestCase):
 
